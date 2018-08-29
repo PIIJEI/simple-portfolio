f55a5faa93f733c09d686283ade502a781243fc3
PIIJEI <piotr.osiak@hotmail.co.uk>
2018-08-29 22:28:09 +0100

    first layout changes

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,9 @@
 //#close_bubble
 //#minimise_bubble_toggle
 
-$(document).ready(function(){
+// $(document).ready(function(){
+(function($) {
+  //chatter box
   $('#toggle_view a').on('click', function(e){
     e.preventDefault();
     $('#chat_bubble').slideToggle(300).toggleClass('bubble-active');
@@ -18,4 +20,37 @@ $(document).ready(function(){
   $('#minimise_bubble_toggle').on('click', function(){
     $('form#post_message').slideToggle(300);
   });
-});
+
+  //counter intro
+  var count = 0;
+  function introCount(e) {
+    if(count === 0) {
+      count++;
+    } else if(count > 100) {
+      count = 100;
+      $('#home_intro').delay(300).fadeOut(100);
+      $('.home_content_container').fadeIn(2000);
+
+    }
+    $('#counter_content').text(count++);
+  }
+  setInterval(introCount, 25);
+
+  //content main index fadeIn when introCount() completed fadeOut
+  $('.home_content_container').hide();
+
+
+    var navSocialIcons = $('#expand-social-toggle');
+    $('.hide-social').hide();
+    $('.top-nav-social').hide();
+
+    navSocialIcons.on('click', function(){
+      console.log('click test');
+      $('.trigger').toggle();
+      $('.top-nav-social').animate({
+        width:'toggle'
+      }, 350);
+    });
+
+
+}(jQuery));
